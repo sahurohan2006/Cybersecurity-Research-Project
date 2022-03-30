5aebab6f68a45ef3eb63f7231f69dd6c82b636ec
Rohan Sahu <sahurohan2006@gmail.com>
2022-03-30 17:15:37 -0400

    The Word class

--- src/Test.java
+++ src/Test.java
@@ -1,38 +1,59 @@
 import java.io.File;
 import java.util.Scanner;
 import java.util.*;
-public class Test {
-    static Map<String,Word> tokensInDoc;
+
+public class Test{
 
     public static void main(String[] args) throws Exception {
+
+        ArrayList<Map<String,Word>> docsStats = new ArrayList<>();
+
         File corpus = new File("/Users/shalinisahu/Desktop/Research Project/corpus1");
         File docs[] = corpus.listFiles();
         for (File f : docs) {
             if (f.isFile()) {
-                calcuatetfIDF(f, docs.length);
+                docsStats.add(calcuatetfIDF(f, docs.length));
             }
         }
-        List<String> vals = new LinkedList<String>(tokensInDoc.keySet());
 
-        Collections.sort(vals, new Comparator<String>() {
-            public int compare(String o1, String o2) {
-                if(tokensInDoc.get(o1).getTFIDF() >= tokensInDoc.get(o2).getTFIDF()) {
+
+        // create a separate method
+
+        for( Map<String, Word> tokensInDoc : docsStats) {
+
+
+            List<String> vals = new LinkedList<String>(tokensInDoc.keySet());
+
+            Collections.sort(vals, new Comparator<String>() {
+                public int compare(String o1, String o2) {
+                    if (tokensInDoc.get(o1).getTFIDF() > tokensInDoc.get(o2).getTFIDF()) {
+                        return -1;
+                    }
+                    if (tokensInDoc.get(o1).getTFIDF() < tokensInDoc.get(o2).getTFIDF()) {
+                        return 1;
+                    }
                     return 0;
                 }
-                return 1;
-            }});
+            });
 
-        Map<String,Word> sorted = new HashMap<>();
-        Iterator it = vals.iterator();
-        while(it.hasNext()) {
-            String temp = (String) it.next();
-            sorted.put(temp, tokensInDoc.get(temp));
+            Map<String, Word> sorted = new HashMap<>();
+            Iterator it = vals.iterator();
+            while (it.hasNext()) {
+                String temp = (String) it.next();
+                sorted.put(temp, tokensInDoc.get(temp));
+            }
+
+            int i = 0;
+            for (String str : sorted.keySet()) {
+                if( i<5 ) {
+                    System.out.println(str + " --> " + sorted.get(str).getTFIDF());
+                    i++;
+                }else{
+                    break;
+                }
+            }
+            System.out.println("----------------");
         }
-        for(String str: sorted.keySet()) {
-            System.out.println(str + " --> " + sorted.get(str).getTFIDF());
-        }
-
-
 
 
 
@@ -172,45 +193,61 @@ public class Test {
          */
     }
 
-    static void calcuatetfIDF(File f, int numFiles) throws Exception {
+    static Map<String, Word> calcuatetfIDF(File f, int numFiles) throws Exception {
         int totalWords = 0;
         Stopwords s = new Stopwords();
-        Set<String> stopwords = s.getWords();
-        tokensInDoc = new HashMap<>();
+
+        //System.out.println(stopwords);
+        Map<String,Word> tokensInDoc = new HashMap<>();
 
         Scanner sc = new Scanner(f);
         Word w;
 
         while (sc.hasNext()) {
             String line = sc.nextLine();
+
+            // remove all not alphanumeric symbols from the line
+            // regular expression
+            line = line.replaceAll("[^a-zA-Z0-9]", " ");
+
+
+
             String[] words = line.split(" ");
+
             for (String x : words) {
+                Set<String> stopwords = s.getWords();
                 // use tokensInDoc to count the number of occurrnces for each word
-                if (!stopwords.contains(x.toLowerCase()) && tokensInDoc.containsKey(x.toLowerCase())) {
+                //x = x.trim();
+
+                if (!s.containsWord(x.toLowerCase()) && tokensInDoc.containsKey(x.toLowerCase()) && x.length() > 0) {
+                    //System.out.println("***" + x + "***" );
                     tokensInDoc.get(x.toLowerCase()).incrementFrequency();
+
                     // tokensInDoc.put(x, tokensInDoc.get(x) + 1);
 
                     //}
-                } else {
-                    w = new Word(x);
+                } else if(!s.containsWord(x.toLowerCase())) {
+                    w = new Word(x.toLowerCase());
                     w.incrementFrequency();
                     w.incrementNumDocsWithWord();
                     tokensInDoc.put(x.toLowerCase(), w);
+                   }
                 }
                 totalWords++;
                 // Loop through the map and create word objects
 
             }
 
-        }
+
         for (String str : tokensInDoc.keySet()) {
-            double tempTF = (double) tokensInDoc.get(str).getFrequency()/totalWords;
+            double tempTF = (double) Math.log( 1 + ((double) tokensInDoc.get(str).getFrequency()/totalWords));
             tokensInDoc.get(str).setTF(tempTF);
-            double tempIDF = Math.log((double) numFiles/(tokensInDoc.get(str).getNumDocsWithWord()+1));
+            double tempIDF = Math.log((double) numFiles/((double) tokensInDoc.get(str).getNumDocsWithWord()+1));
             tokensInDoc.get(str).setIDF(tempIDF);
             tokensInDoc.get(str).setTFIDF(tempTF/tempIDF);
         }
 
+        return tokensInDoc;
 
     }
 
